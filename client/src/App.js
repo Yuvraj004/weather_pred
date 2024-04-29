106d201e62a3eb9bd3fbfa8331e38ce9e88f47aa
Yuvraj004 <yuvrajchat@gmail.com>
2024-04-30 05:04:09 +0530

    frontend created

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <nav className="navbar bg-body-tertiary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Logo" width="30" height="24" className="d-inline-block align-text-top"/>
+            Bootstrap
+          </a>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
